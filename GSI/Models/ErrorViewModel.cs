7d079090d6999ce2e337f657becb4ea24cb612db
Aplo612 <mauro_mauro_16@hotmail.com>
2023-11-20 17:46:44 -0500

    Agregar archivos de proyecto.

---
+++ GSI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GSI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
